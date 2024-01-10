98d6f9eccbc4b394688f3903cb4da37114938879
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-01-11 04:06:34 +0530

    Done some updation
    Signed-off-by: aryansharma9917 <sharmaaryan9837@gmail.com>

--- cmd/docker/container/helper.go
+++ cmd/docker/container/helper.go
@@ -7,18 +7,21 @@ import (
 	"github.com/docker/docker/client"
 )
 
+// dockerClient returns a docker client
 func dockerClient() (context.Context, *client.Client) {
 	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv)
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	checkErr(err)
 	defer cli.Close()
 
 	return ctx, cli
 }
 
-// runningContainerList returns a list of rumming containers
+// runnnigContainerList returns a list of running containers
 func runnnigContainerList(cli *client.Client, ctx context.Context) []types.Container {
-	containerList, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+
+	containerList, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	checkErr(err)
 	return containerList
+
 }
