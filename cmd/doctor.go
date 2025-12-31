5b41b5d922824928c1d66c72e709e1b792e5f4b2
aryansharma9917 <sharmaaryan9837@gmail.com>
2025-12-31 23:38:56 +0530

    Updated Docker command

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"os/exec"
 	"runtime"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,12 +16,35 @@ var doctorCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Codewise CLI Doctor")
 		fmt.Println("-------------------")
+
+		// Go
 		fmt.Println("Go version:", runtime.Version())
+
+		// OS
 		fmt.Println("OS/Arch:", runtime.GOOS, runtime.GOARCH)
+
+		// Codewise
 		fmt.Println("Codewise version:", rootCmd.Version)
+
+		// Docker
+		checkDocker()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(doctorCmd)
+}
+
+func checkDocker() {
+	cmd := exec.Command("docker", "--version")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Docker: not installed")
+		return
+	}
+
+	fmt.Println("Docker:", strings.TrimSpace(out.String()))
 }
