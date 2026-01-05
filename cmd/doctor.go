5c63958a529e21cb241f3e6a44ef26d29d51eab5
aryansharma9917 <sharmaaryan9837@gmail.com>
2026-01-05 23:27:36 +0530

    Added Docker init command

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -2,49 +2,30 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"runtime"
 
+	"github.com/aryansharma9917/codewise-cli/pkg/docker"
 	"github.com/spf13/cobra"
 )
 
-var doctorCmd = &cobra.Command{
-	Use:   "doctor",
-	Short: "Check Codewise CLI environment",
+var dockerCmd = &cobra.Command{
+	Use:   "docker",
+	Short: "Docker related helpers",
+}
+
+var dockerInitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Generate a Dockerfile",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Codewise CLI Doctor")
-		fmt.Println("-------------------")
-
-		// Go info
-		fmt.Println("Go version:", runtime.Version())
-		fmt.Println("OS/Arch:", runtime.GOOS, runtime.GOARCH)
-
-		// Codewise version
-		fmt.Println("Codewise version:", rootCmd.Version)
-
-		// Working directory
-		if wd, err := os.Getwd(); err == nil {
-			fmt.Println("Working directory:", wd)
+		err := docker.InitDockerfile()
+		if err != nil {
+			fmt.Println("ℹ️", err.Error())
+			return
 		}
-
-		// Git version
-		if out, err := exec.Command("git", "--version").Output(); err == nil {
-			fmt.Println("Git:", string(out))
-		} else {
-			fmt.Println("Git: not found")
-		}
-
-		// Config path (future use)
-		configPath := os.ExpandEnv("$HOME/.codewise/config.yaml")
-		if _, err := os.Stat(configPath); err == nil {
-			fmt.Println("Config file:", configPath)
-		} else {
-			fmt.Println("Config file:", configPath, "(not found)")
-		}
+		fmt.Println("✅ Dockerfile created")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(doctorCmd)
+	dockerCmd.AddCommand(dockerInitCmd)
+	rootCmd.AddCommand(dockerCmd)
 }
