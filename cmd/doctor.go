7ffbfd254a776b2703e6345718748b15f4c71f46
aryansharma9917 <sharmaaryan9837@gmail.com>
2026-01-06 23:04:55 +0530

    add docker commands to CLI

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var imageTag string
+
 var dockerCmd = &cobra.Command{
 	Use:   "docker",
 	Short: "Docker related helpers",
@@ -16,8 +18,7 @@ var dockerInitCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Generate a Dockerfile",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := docker.InitDockerfile()
-		if err != nil {
+		if err := docker.InitDockerfile(); err != nil {
 			fmt.Println("ℹ️", err.Error())
 			return
 		}
@@ -25,7 +26,38 @@ var dockerInitCmd = &cobra.Command{
 	},
 }
 
+var dockerValidateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "Validate Dockerfile best practices",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := docker.ValidateDockerfile(); err != nil {
+			fmt.Println("❌", err.Error())
+		}
+	},
+}
+
+var dockerBuildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Build Docker image",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := docker.BuildDockerImage(imageTag); err != nil {
+			fmt.Println("❌ Docker build failed")
+		}
+	},
+}
+
 func init() {
+	dockerBuildCmd.Flags().StringVarP(
+		&imageTag,
+		"tag",
+		"t",
+		"",
+		"Docker image tag (default: codewise:latest)",
+	)
+
 	dockerCmd.AddCommand(dockerInitCmd)
+	dockerCmd.AddCommand(dockerValidateCmd)
+	dockerCmd.AddCommand(dockerBuildCmd)
+
 	rootCmd.AddCommand(dockerCmd)
 }
