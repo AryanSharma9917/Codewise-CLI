aee38b2950582adc15e2c8bb9fe1c2c78bb0eb18
aryansharma9917 <sharmaaryan9837@gmail.com>
2026-01-14 00:25:08 +0530

    Added flag cmd

--- cmd/k8s.go
+++ cmd/k8s.go
@@ -1,42 +1,12 @@
 package cmd
 
-import (
-	"fmt"
-
-	"github.com/aryansharma9917/codewise-cli/pkg/k8s"
-	"github.com/spf13/cobra"
-)
-
-var (
-	k8sAppName string
-	k8sImage   string
-)
+import "github.com/spf13/cobra"
 
 var k8sCmd = &cobra.Command{
 	Use:   "k8s",
 	Short: "Kubernetes helpers",
 }
 
-var k8sInitCmd = &cobra.Command{
-	Use:   "init",
-	Short: "Generate Kubernetes manifests",
-	Run: func(cmd *cobra.Command, args []string) {
-		err := k8s.InitK8sManifests(k8s.Options{
-			AppName: k8sAppName,
-			Image:   k8sImage,
-		})
-		if err != nil {
-			fmt.Println("ℹ️", err.Error())
-			return
-		}
-		fmt.Println("✅ Kubernetes manifests created in ./k8s/app")
-	},
-}
-
 func init() {
-	k8sInitCmd.Flags().StringVar(&k8sAppName, "app", "", "Application name")
-	k8sInitCmd.Flags().StringVar(&k8sImage, "image", "", "Docker image name")
-
-	k8sCmd.AddCommand(k8sInitCmd)
 	rootCmd.AddCommand(k8sCmd)
 }
