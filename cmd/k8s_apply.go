479232d58b5b93ae8e37a485714e12441ed04f7c
aryansharma9917 <sharmaaryan9837@gmail.com>
2026-01-17 16:21:08 +0530

    add dry run flag to k8s apply command

--- cmd/k8s_apply.go
+++ cmd/k8s_apply.go
@@ -10,23 +10,26 @@ import (
 var (
 	k8sNamespace string
 	k8sContext   string
+	k8sDryRun    bool
 )
 
 var k8sApplyCmd = &cobra.Command{
 	Use:   "apply",
 	Short: "Apply Kubernetes manifests to the current cluster",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := k8s.CheckKubectl(); err != nil {
-			fmt.Println("info:", err.Error())
-			return
+		// If not a dry run, check cluster connectivity
+		if !k8sDryRun {
+			if err := k8s.CheckKubectl(); err != nil {
+				fmt.Println("info:", err.Error())
+				return
+			}
+			if err := k8s.CheckCluster(); err != nil {
+				fmt.Println("info:", err.Error())
+				return
+			}
 		}
 
-		if err := k8s.CheckCluster(); err != nil {
-			fmt.Println("info:", err.Error())
-			return
-		}
-
-		if err := k8s.ApplyManifests(k8sNamespace, k8sContext); err != nil {
+		if err := k8s.ApplyManifests(k8sNamespace, k8sContext, k8sDryRun); err != nil {
 			fmt.Println("info:", err.Error())
 			return
 		}
@@ -36,6 +39,6 @@ var k8sApplyCmd = &cobra.Command{
 func init() {
 	k8sApplyCmd.Flags().StringVar(&k8sNamespace, "namespace", "", "Kubernetes namespace for deployment")
 	k8sApplyCmd.Flags().StringVar(&k8sContext, "context", "", "Kubernetes context for deployment")
-
+	k8sApplyCmd.Flags().BoolVar(&k8sDryRun, "dry-run", false, "Preview changes without applying them")
 	k8sCmd.AddCommand(k8sApplyCmd)
 }
